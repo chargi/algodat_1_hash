357e07db67e8295f96b66a9a330b02346e4a7e60
chargi <chargi@gmail.com>
2015-03-24 21:39:42 +0100

    1st

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * Algorithmen und Datenstrukturen
+ * 
+ * Exercise 1: Hash Table - Aktien
+ * 
+ * Team members: Markus Richter (if14b006)
+ *               Marco Kretschmer (if14b028),
+ * 		 
+ * 
+ * File: main.cpp
+ * 
+ * Description:
+ * This file contains the main() procedure
+ * 
+ 
+ * */
+
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
