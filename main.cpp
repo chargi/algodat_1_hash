a075d555ea066caec197473cbe4b2ba94866a24a
gouki <gouki@127.0.53.53>
2015-04-04 00:43:10 +0200

    Hier das normale Rahmenprogramm mit unfertiger Menüführung. Würdest du das ausbauen? Mein Gedanke ist der dass der User sowohl über die Nummer als auch über den Namen des Befehls den jeweiligen Befehl ausführen kann. Dazu muss allerdings der Userinput entweder komplett tolower oder toupper gemacht werden, damit sich das vernünftig überprüfen lässt. Ich glaube der Code spricht für sich.

--- main.cpp
+++ main.cpp
@@ -12,18 +12,58 @@
  * Description:
  * This file contains the main() procedure
  * 
- 
- * */
+ */
 
-#include <cstdlib>
-
-// Das ist ein Test ob Git Push funktioniert.
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    cout << "checked out" << endl;
-    cout << "checked out TEST2" << endl;
+// Function prototypes
+void showMenu(); // Show the main menu.
+string promptUser(); // Prompt the user for input.
+
+/* The framework application in which the code for hashtable, etc. is
+ * later to be inserted.
+ * 
+ */
+int main()
+{
+    string menuChoice;
+    
+    do
+    {
+        // Show main menu, prompt user for input and then act accordingly.
+        showMenu();
+        menuChoice = promptUser();
+        
+    }while( menuChoice != "quit" && menuChoice != "8" );
+    
     return 0;
 }
 
+// Display the main menu.
+//
+void showMenu()
+{
+    cout << "ALGODAT - Aktienverwaltung\n\n"
+         << "1. ADD: Neue Aktie hinzufuegen\n"
+         << "2. DEL: Aktie loeschen\n"
+         << "3. IMPORT: Kurswerte aus CSV-Datei importieren\n"
+         << "4. SEARCH: Aktie suchen (Name oder Kuerzel)\n"
+         << "5. PLOT: Schlusskurse der letzten 30 Tage anzeigen\n"
+         << "6. SAVE <Dateiname>: Hashtable speichern\n"
+         << "7. LOAD <Dateiname>: Hashtable laden\n"
+         << "8. QUIT: Programm beenden\n\n"
+         << "Auswahl (Nummer oder Befehl): ";
+}
+
+
+// Prompt the user for input and return it. Process elsewhere.
+//
+string promptUser()
+{
+    string tmp;
+    getline(cin, tmp, '\n');
+    return tmp;
+}
